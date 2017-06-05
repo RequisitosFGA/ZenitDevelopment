90cfa97a67c0a61ec9ad585d6cbcedd8bed2345e
Tulio Coelho <tulioopc@gmail.com>
2017-06-05 13:26:56 -0300

    Adicionando clientes e criando página incial da aplicação (Login)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170604204544) do
+ActiveRecord::Schema.define(version: 20170605153939) do
+
+  create_table "customers", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
